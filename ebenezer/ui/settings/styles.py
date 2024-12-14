2ec17b032d668e97bb95e652619bb3f2a97ae098
William Sena <me@willsena.dev>
2024-12-14 00:20:11 -0300

    fix: group styles

--- ebenezer/ui/settings/styles.py
+++ ebenezer/ui/settings/styles.py
@@ -1,3 +1,4 @@
+from tkinter import ttk
 from typing import NamedTuple
 
 from ebenezer.config.settings import AppSettings
@@ -39,3 +40,49 @@ def build_fonts(settings: AppSettings):
         bold_medium=FontStyle(family=settings.fonts.font, size=20, style="bold"),
         bold_big=FontStyle(family=settings.fonts.font, size=30, style="bold"),
     )
+
+
+def build_style(settings: AppSettings, theme_name: str, root: ttk.Frame) -> ttk.Style:
+    fonts = build_fonts(settings)
+    style = ttk.Style()
+    style.theme_use(theme_name)
+
+    style.configure("TEntry", font=fonts.big)
+
+    style.configure(
+        "Form.TEntry",
+        font=fonts.medium,
+        padding=5,
+    )
+
+    style.configure(
+        "Form.TCombobox",
+        font=fonts.medium,
+        padding=5,
+    )
+
+    style.map("Form.TCombobox", font=[("readonly", fonts.medium)])
+
+    style.configure(
+        "Sidebar.TFrame",
+        foreground=settings.colors.fg_white,
+    )
+
+    style.configure(
+        "Tab.TLabel",
+        foreground=settings.colors.fg_gray,
+        font=fonts.regular,
+        padding=10,
+    )
+    style.configure(
+        "TabSelected.TLabel",
+        font=fonts.medium,
+        foreground=settings.colors.fg_white,
+        padding=10,
+    )
+
+    style.configure(".", font=fonts.regular)
+
+    root.option_add("*TCombobox*Listbox.font", fonts.regular)
+
+    return style
