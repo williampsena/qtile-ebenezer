bf6b1a614030334b70af7c6e0daf83ef23c3931e
William Sena <me@willsena.dev>
2024-12-24 18:13:33 -0300

    Bump version: 0.1.17 → 0.1.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="qtile-ebenezer",
-    version="0.1.17",
+    version="0.1.18",
     description="This repository provides a collection of widgets and behaviors spanning Desktop to Qtile Tiling Window Manager.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
