5b971868c0d931ccc324cd734fb8c62fd8a1e154
William Sena <me@willsena.dev>
2025-03-23 18:15:08 -0300

    feat: upgrade

--- stubs/ebenezer/core/layout.pyi
+++ stubs/ebenezer/core/layout.pyi
@@ -1,9 +1,8 @@
-from typing import Any, Callable
-
+from ebenezer.config.settings import AppSettings as AppSettings, load_settings_by_files as load_settings_by_files
 from libqtile.layout.base import Layout as BaseLayout
-
-from ebenezer.config.settings import AppSettings as AppSettings
+from typing import Any, Callable
 
 LAYOUTS: dict[str, Callable[[AppSettings, dict[str, Any]], BaseLayout]]
 
 def build_layouts(settings: AppSettings): ...
+def set_floating_window(window) -> None: ...
