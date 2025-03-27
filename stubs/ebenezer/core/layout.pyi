af24d89fafb9cc77ae174531d90033ae33cec67d
William Sena <me@willsena.dev>
2025-03-27 08:29:09 -0300

    feat: upgrade

--- stubs/ebenezer/core/layout.pyi
+++ stubs/ebenezer/core/layout.pyi
@@ -1,11 +1,12 @@
+from _typeshed import Incomplete
+from ebenezer.config.settings import AppSettings as AppSettings, load_settings_by_files as load_settings_by_files
+from libqtile.layout.base import Layout as BaseLayout
 from typing import Any, Callable
 
-from libqtile.layout.base import Layout as BaseLayout
-
-from ebenezer.config.settings import AppSettings as AppSettings
-from ebenezer.config.settings import load_settings_by_files as load_settings_by_files
-
+CENTER_WINDOWS_TITLES: Incomplete
 LAYOUTS: dict[str, Callable[[AppSettings, dict[str, Any]], BaseLayout]]
 
 def build_layouts(settings: AppSettings): ...
 def set_floating_window(window) -> None: ...
+def centralize_window(settings: AppSettings, window): ...
+def is_ebenezer_window(window): ...
--- stubs/ebenezer/core/theme.pyi
+++ stubs/ebenezer/core/theme.pyi
@@ -1,10 +1,8 @@
-from typing import NamedTuple
-
-from ebenezer.config.settings import AppSettings as AppSettings
-from ebenezer.config.settings import AppSettingsColors as AppSettingsColors
+from ebenezer.config.settings import AppSettings as AppSettings, AppSettingsColors as AppSettingsColors
 from ebenezer.core.dict import merge_dicts_recursive as merge_dicts_recursive
 from ebenezer.core.files import resolve_file_path as resolve_file_path
 from ebenezer.core.yaml import read_yaml_file as read_yaml_file
+from typing import NamedTuple
 
 class RofiTemplate(NamedTuple):
     template: str
