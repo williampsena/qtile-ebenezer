4222db79cb606c9e4ac37c6997057a65091e2ba0
William Sena <me@willsena.dev>
2025-04-03 06:23:41 -0300

    feat: upgrade

--- stubs/ebenezer/core/layout.pyi
+++ stubs/ebenezer/core/layout.pyi
@@ -1,10 +1,8 @@
-from typing import Any, Callable
-
 from _typeshed import Incomplete
+from ebenezer.config.settings import AppSettings as AppSettings, load_settings_by_files as load_settings_by_files
+from ebenezer.widgets.helpers.args import build_widget_args as build_widget_args
 from libqtile.layout.base import Layout as BaseLayout
-
-from ebenezer.config.settings import AppSettings as AppSettings
-from ebenezer.config.settings import load_settings_by_files as load_settings_by_files
+from typing import Any, Callable
 
 CENTER_WINDOWS_TITLES: Incomplete
 LAYOUTS: dict[str, Callable[[AppSettings, dict[str, Any]], BaseLayout]]
